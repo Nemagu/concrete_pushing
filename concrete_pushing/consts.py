1e3fa917eb5f45339c6812407d35e891e198c190
VladimirGerman <vladimir.german.and@yandex.ru>
2024-10-09 22:25:09 +0700

    Попытался улучшить решение для констант

--- concrete_pushing/consts.py
+++ concrete_pushing/consts.py
@@ -1,64 +1,150 @@
+SYSTEMS_MEASUREMENT: dict[str, float] = {
+    'СИ': 1.0,
+    'СГС': 0.10197162129779283,
+}
+
+CONCRETE_CLASSES: tuple[str] = (
+    'B10',
+    'B15',
+    'B20',
+    'B25',
+    'B30',
+    'B35',
+    'B40',
+    'B45',
+    'B50',
+    'B55',
+    'B60',
+)
+
+REINFORCEMENT_CLASSES: tuple[str] = (
+    'A240',
+    'A300',
+    'A400',
+    'A500',
+    'A600',
+    'A800',
+    'A1000',
+    'B500',
+)
+
+CONCRETE_NORMAL_AXIAL_COMPRESSION: dict[str, int] = {
+    'B10': 850000,
+    'B15': 1100000,
+    'B20': 1350000,
+    'B25': 1550000,
+    'B30': 1750000,
+    'B35': 1950000,
+    'B40': 2100000,
+    'B45': 2250000,
+    'B50': 2450000,
+    'B55': 2600000,
+    'B60': 2750000,
+}
+
+REINFORCEMENT_NORMAL_STRETCHING: dict[str, int] = {
+    'A240': 210000000,
+    'A300': 270000000,
+    'A400': 350000000,
+    'A500': 435000000,
+    'A600': 520000000,
+    'A800': 695000000,
+    'A1000': 870000000,
+    'B500': 435000000,
+}
+
+
 class ConcreteConsts:
-    __RBTNB10 = 0.85
-    __RBTNB15 = 1.1
-    __RBTNB20 = 1.35
-    __RBTNB25 = 1.55
-    __RBTNB30 = 1.75
-    __RBTNB35 = 1.95
-    __RBTNB40 = 2.1
-    __RBTNB45 = 2.25
-    __RBTNB50 = 2.45
-    __RBTNB55 = 2.6
-    __RBTNB60 = 2.75
-    
-    @classmethod
-    def get_rbtn(cls, concrete_class: str=None) -> float:
+    def __init__(
+        self,
+        concrete_class: str,
+        system_measurement: str = 'СИ',
+        value_unit: float = 0.000001,
+    ):
         """
-        Получение нормативного осевого растяжения.\n
         Аргументы:\n
-        concrete_class - класс бетона в виде строки \'B20\'.
+        concrete_class - класс бетона в виде строки \'B20\';\n
+        system_measurement - система измерения. По умолчанию СИ, доступна
+        СГС;\n
+        value_unit - коэффициент преобразования единиц: 0.001 - кило,
+        0.000001 - мега.
         """
-        rbtn = getattr(cls, f'__RBTN{concrete_class}', None)
-        if rbtn is None:
-            raise KeyError(f'Такого класса бетона нет: {concrete_class}')
-        return rbtn
-    
-    @classmethod
-    def get_rbt(cls, concrete_class: str, ybi: float) -> float:
+        self.system_measurement = system_measurement
+        self.concrete_class = concrete_class
+        self.value_unit = value_unit
+        self.data_validation()
+        self._set_rbtn()
+
+    def data_validation(self):
+        if self.__dict__.get('concrete_class') not in CONCRETE_CLASSES:
+            raise KeyError('Такой класс бетона не найден.')
+
+        if (self.__dict__.get('system_measurement')
+                not in SYSTEMS_MEASUREMENT.keys()):
+            raise KeyError('Такая система измерения не поддерживается.')
+
+    def _set_rbtn(self):
+        self._rbtn = CONCRETE_NORMAL_AXIAL_COMPRESSION.get(
+            self.concrete_class
+        )
+
+    def get_rbtn(self) -> float:
+        """
+        Получение нормативного осевого растяжения.
+        """
+        return self._rbtn * self.value_unit
+
+    def get_rbt(self, ybi: float = 0.9) -> float:
         """
         Получение расчетного осевого растяжения.\n
         Аргументы:\n
-        concrete_class - класс бетона в виде строки \'B20\';\n
         ybi - коэффициент условий работы бетона.
         """
-        return cls.get_rbtn(concrete_class=concrete_class) * ybi
+        return self.get_rbtn() * ybi
 
 
 class ReinforcementConsts:
-    __RSA240 = 215
-    __RSA300 = 270
-    __RSA400 = 355
-    __RSA500 = 435
-    
-    @classmethod
-    def get_rs(cls, reinforcement_class: str) -> float:
+    def __init__(
+        self,
+        reinforcement_class: str,
+        unit_measurement: str = 'СИ',
+        value_unit: float = 0.000001,
+    ):
         """
-        Получение нормативного продольного растяжения.\n
         Аргументы:\n
-        reinforcement_class - класс арматуры в виде строки \'A240\'.
+        reinforcement_class - класс арматуры в виде строки \'A240\';\n
+        unit_measurement - система измерения. По умолчанию СИ, доступна СГС;\n
+        value_unit - коэффициент преобразования единиц: 0.001 - кило,
+        0.000001 - мега.
         """
-        rs = getattr(cls, f'__RS{reinforcement_class}', None)
-        if rs is None:
-            raise KeyError(f'Такого класса арматуры нет: {reinforcement_class}')
-        return rs
-    
-    @classmethod
-    def get_rsw(cls, reinforcement_class: str, ybi: float = 0.8) -> float:
+        self.reinforcement_class = reinforcement_class
+        self.value_unit = value_unit
+        self.data_validation()
+        self._set_rs()
+
+    def data_validation(self):
+        if self.__dict__.get('concrete_class') not in REINFORCEMENT_CLASSES:
+            raise KeyError('Такой класс арматуры не найден.')
+
+        if (self.__dict__.get('system_measurement')
+                not in SYSTEMS_MEASUREMENT.keys()):
+            raise KeyError('Такая система измерения не поддерживается.')
+
+    def _set_rs(self):
+        self._rs = REINFORCEMENT_NORMAL_STRETCHING.get(
+            self.reinforcement_class
+        )
+
+    def get_rs(self) -> float:
+        """
+        Получение нормативного продольного растяжения.
+        """
+        return self._rs * self.value_unit
+
+    def get_rsw(self, ybi: float = 1.0) -> float:
         """
         Получение расчетного осевого растяжения.\n
         Аргументы:\n
-        reinforcement_class - класс арматуры в виде строки \'A240\';\n
         ybi - коэффициент условий работы арматуры.
         """
-        return cls.get_rs(reinforcement_class=reinforcement_class) * ybi
-        
+        return self.get_rs() * ybi
